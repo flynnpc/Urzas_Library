2455f4a436a43e44d31c17fa4981788536be3584
flynnpc <paul.flynn@utexas.edu>
2018-04-21 10:07:53 -0500

    New release - version 2.0 using OOP
    This release utilizes UrzasPress class to create card Magic the
    Gathering card objects. These card objects can be possess various card
    components. Class methods include:
    
    card_name: Returns a string with the name of the cardUrl
    
    mana_cost: Returns a list of mana cost. Colorless mana are totaled and
    returned as strings. Color mana is represented on a one to one basis,
    i.e. (blue, blue, 3) indicate two blue mana and three colorless
    
    mana_cost_convert: Returns an int and calculates the total number of all
    mana required
    
    card_type: Returns a string of the card's type_label
    
    card_text: Returns a list with the card's rules related to game play
    
    flavor_text: Returns a list with the card's non-rule related flavor
    text, if any.
    
    expansion: Returns a string of the card's expansion set
    
    rarity: returns a string of the card's rare value i.e. common, uncommon,
    rare, or mythic.
    
    card_number: Returns a string of the card's collection number within
    that set
    
    artist: Returns a string of the referenced card's artist. Some cards
    have two versions with different artists
    
    * UrzasPress requires Beautiful Soup, requests, and re libraries

---
+++ Urza.py.py
@@ -0,0 +1,85 @@
+from bs4 import BeautifulSoup
+import requests, re
+
+class UrzasPress:
+
+    def __init__(self, cardUrlId):
+        self.id = str(cardUrlId)
+        self.cardUrl = requests.get(
+        'http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid='+
+        self.id)
+        self.soup = BeautifulSoup(self.cardUrl.text,'lxml')
+
+    def card_name(self):
+        if self.soup.find("div", string=re.compile("Card Name:")):
+            self.card_label = self.soup.find("div", string=re.compile(
+                                                        "Card Name:"))
+            return self.card_label.find_next_sibling('div').get_text().strip()
+        return
+    def mana_cost(self):
+        if self.soup.find("div", string=re.compile("Mana Cost:")):
+            self.mana_label = self.soup.find("div", string=re.compile(
+                                                        "Mana Cost:"))
+            self.mana_symbols = self.mana_label.find_next_sibling(
+                                            'div').find_all("img")
+            self.mana_cost = [mana['alt'] for mana in self.mana_symbols]
+            return self.mana_cost
+        return
+    def mana_cost_convert(self):
+        if self.soup.find("div", string=re.compile("Converted Mana Cost:")):
+            self.converted_label = self.soup.find("div", string=re.compile(
+                                                    "Converted Mana Cost:"))
+            self.total_mana = self.converted_label.find_next_sibling(
+                                            'div').get_text().strip()
+            return int(self.total_mana)
+        return
+    def card_type(self):
+        if self.soup.find("div", string=re.compile("Types:")):
+            self.type_label = self.soup.find("div", string=re.compile("Types:"))
+            self.card_type = self.type_label.find_next_sibling(
+                                        'div').get_text().strip()
+            return self.card_type
+        return
+    def card_text(self):
+        if self.soup.select(".cardtextbox"):
+            self.text_label = self.soup.select(".cardtextbox")
+            self.card_txt = [text.get_text() for text in self.text_label]
+            return self.card_txt
+        return
+    def flavor_text(self):
+        if self.soup.select(".flavortextbox"):
+            self.flavor_label = self.soup.select(".flavortextbox")
+            self.flavor_txt = [line.get_text().strip() for line in self.flavor_label]
+            return self.flavor_txt
+        return
+    def expansion(self):
+        if self.soup.find("div", string=re.compile(
+                "Expansion:")).find_next_sibling():
+            self.set_label = self.soup.find("div", string=re.compile(
+                                    "Expansion:")).find_next_sibling()
+            self.expansion_set = self.set_label.img["title"]
+            return self.expansion_set
+        return
+    def rarity(self):
+        if self.soup.find("div", string=re.compile("Rarity:")):
+            self.rarity_label = self.soup.find("div", string=re.compile(
+                                                            "Rarity:"))
+            self.rare = self.rarity_label.find_next_sibling('div').get_text()
+            return self.rare
+        return
+    def card_number(self):
+        if self.soup.find("div", string=re.compile("Card Number:")):
+            self.number_label = self.soup.find("div", string=re.compile(
+                                                        "Card Number:"))
+            self.number = self.number_label.find_next_sibling(
+                                        'div').get_text().strip()
+            return self.number
+        return
+    def artist(self):
+        if self.soup.find("div", string=re.compile("Artist:")):
+            self.artist_label = self.soup.find("div", string=re.compile(
+                                                                "Artist:"))
+            self.art = self.artist_label.find_next_sibling(
+                                    'div').get_text().strip()
+            return self.art
+        return
