2763aa5942294f33cce5c8a9f7600c2440a6768e
flynnpc <paul.flynn@utexas.edu>
2018-05-30 06:27:46 -0500

    Version 2.0.1 - bug fixes
    AttributeError
    Beautifulsoup
    to account for unglued cards
    connections

--- Urza.py
+++ Urza.py
@@ -1,90 +1,113 @@
 from bs4 import BeautifulSoup
 import requests, re
-
+# update 01 - replaced if beautiful soup object present with try except AttributeError
+# update 02 - replaced soup.text with soup.content to for easier use in Beautifulsoup
+# update 03 - replaced int with float declaration in mana_cost_convert to account for unglued cards
+# update 04 - updated expansion function to account for missing img
 class UrzasPress:
 
     def __init__(self, cardUrlId):
         self.id = str(cardUrlId)
+        self.win10Head = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64;rv:60.0) Gecko/20100101 Firefox/60.0'}
         self.cardUrl = requests.get(
         'http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid='+
-        self.id)
-        self.soup = BeautifulSoup(self.cardUrl.text,'lxml')
+        self.id, headers=self.win10Head, timeout=30)
+        self.soup = BeautifulSoup(self.cardUrl.content,'lxml')
+    def checkUrl(self):
+        if self.soup.select('.introheader'):
+            return False
+        else:
+            return True
     def card_name(self):
-        if self.soup.find("div", string=re.compile("Card Name:")):
-            self.card_label = self.soup.find("div", string=re.compile(
+        try:
+            self.name_div = self.soup.find("div", string=re.compile(
                                                         "Card Name:"))
-            return self.card_label.find_next_sibling('div').get_text().strip()
-        return
+            return self.name_div.find_next_sibling('div').get_text().strip()
+        except AttributeError:
+            return
     def mana_cost(self):
-        if self.soup.find("div", string=re.compile("Mana Cost:")):
+        try:
             self.mana_label = self.soup.find("div", string=re.compile(
                                                         "Mana Cost:"))
-            self.mana_symbols = self.mana_label.find_next_sibling(
+            self.mana_flash = self.mana_label.find_next_sibling(
                                             'div').find_all("img")
-            self.mana_cost = [mana['alt'] for mana in self.mana_symbols]
+            self.mana_cost = [self.mana['alt'] for self.mana in self.mana_flash]
             return self.mana_cost
-        return
+        except AttributeError:
+            return
     def mana_cost_convert(self):
-        if self.soup.find("div", string=re.compile("Converted Mana Cost:")):
+        try:
             self.converted_label = self.soup.find("div", string=re.compile(
                                                     "Converted Mana Cost:"))
             self.total_mana = self.converted_label.find_next_sibling(
                                             'div').get_text().strip()
-            return int(self.total_mana)
-        return
+            return float(self.total_mana)
+        except AttributeError:
+            return
     def card_type(self):
-        if self.soup.find("div", string=re.compile("Types:")):
+        try:
             self.type_label = self.soup.find("div", string=re.compile("Types:"))
             self.card_type = self.type_label.find_next_sibling(
                                         'div').get_text().strip()
             return self.card_type
-        return
+        except AttributeError:
+            return
     def card_text(self):
-        if self.soup.select(".cardtextbox"):
-            self.text_label = self.soup.select(".cardtextbox")
+        try:
+            self.text_div = self.soup.select(".cardtextbox")
             self.card_txt = []
-            for self.text_box in self.text_label:
-                self.symbols = self.text_box.find_all('img')
+            for self.flash_text_box in self.text_div:
+                self.symbols = self.flash_text_box.find_all('img')
                 self.alt_List = []
-                for self.alt_text in self.symbols:
-                    self.alt_List.append(self.alt_text['alt'])
-                self.alt_List.append(self.text_box.get_text().strip())
+                for self.alt_flash in self.symbols:
+                    self.alt_List.append(self.alt_flash['alt'])
+                self.alt_List.append(self.flash_text_box.get_text().strip())
                 self.card_txt.append(self.alt_List)
             return self.card_txt
+        except AttributeError:
+            return
     def flavor_text(self):
-        if self.soup.select(".flavortextbox"):
-            self.flavor_label = self.soup.select(".flavortextbox")
-            self.flavor_txt = [line.get_text().strip() for line in self.flavor_label]
+        try:
+            self.flash_flavor_label = self.soup.select(".flavortextbox")
+            self.flavor_txt = [self.line.get_text().strip()
+                                        for self.line in self.flash_flavor_label]
             return self.flavor_txt
-        return
+        except AttributeError:
+            return
     def expansion(self):
-        if self.soup.find("div", string=re.compile(
-                "Expansion:")).find_next_sibling():
+        try:
             self.set_label = self.soup.find("div", string=re.compile(
                                     "Expansion:")).find_next_sibling()
-            self.expansion_set = self.set_label.img["title"]
+            # self.expansion_set = self.set_label.img["title"]
+            self.expansion_set = self.set_label.get_text().strip()
             return self.expansion_set
-        return
+        except AttributeError:
+            return
     def rarity(self):
-        if self.soup.find("div", string=re.compile("Rarity:")):
+        try:
             self.rarity_label = self.soup.find("div", string=re.compile(
                                                             "Rarity:"))
-            self.rare = self.rarity_label.find_next_sibling('div').get_text()
+            self.rare = self.rarity_label.find_next_sibling(
+                                        'div').get_text().strip()
             return self.rare
-        return
+        except AttributeError:
+            return
     def card_number(self):
-        if self.soup.find("div", string=re.compile("Card Number:")):
+        try:
             self.number_label = self.soup.find("div", string=re.compile(
                                                         "Card Number:"))
             self.number = self.number_label.find_next_sibling(
                                         'div').get_text().strip()
             return self.number
-        return
+        except AttributeError:
+            return
     def artist(self):
-        if self.soup.find("div", string=re.compile("Artist:")):
+        try:
             self.artist_label = self.soup.find("div", string=re.compile(
                                                                 "Artist:"))
             self.art = self.artist_label.find_next_sibling(
                                     'div').get_text().strip()
             return self.art
-        return
+        except AttributeError:
+            return
+    
